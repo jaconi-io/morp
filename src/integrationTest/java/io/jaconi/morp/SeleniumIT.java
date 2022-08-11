b356ec5b1d3b821d178dc9819844296b7039979b
Jan Fiedler <jfiedler@jaconi.io>
2022-08-11 11:50:23 +0200

    test: extend selenium test with host based tenant identification

--- src/integrationTest/java/io/jaconi/morp/SeleniumIT.java
+++ src/integrationTest/java/io/jaconi/morp/SeleniumIT.java
@@ -2,18 +2,19 @@ package io.jaconi.morp;
 
 
 import dasniko.testcontainers.keycloak.KeycloakContainer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.model.MediaType;
 import org.mockserver.verify.VerificationTimes;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.*;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
@@ -32,13 +33,11 @@ import static org.assertj.core.api.Assertions.fail;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
-// TODO run morp as container inside TestContainer setup
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@ActiveProfiles({"test", "wiretap"})
-//@TestPropertySource(properties = "keycloak.host=host.docker.internal")
+/**
+ * This integration tests checks whether a Morp protected backend (mockserver) can be accesses via various identity
+ * providers (as defined in application-selenium.yaml)
+ */
 public class SeleniumIT {
-
-    private static Logger log = LoggerFactory.getLogger(SeleniumIT.class);
 
     private static BrowserWebDriverContainer seleniumContainer;
     private static KeycloakContainer keycloakContainer;
@@ -49,15 +48,12 @@ public class SeleniumIT {
     private RemoteWebDriver driver;
 
     @BeforeAll
-    // TODO run Keycloak as container in the same network
-    // TODO run Morp as container in the same network
     static void setup() {
 
         // we need a network for our containers to see each other
         Network network = Network.newNetwork();
 
-
-        // start Selenium in that network
+        // start Selenium with a Chrome browser
         seleniumContainer = new BrowserWebDriverContainer(
                 ArmUtil.select("seleniarm/standalone-chromium", "selenium/standalone-chrome:latest"))
                 .withCapabilities(new ChromeOptions());
@@ -82,8 +78,7 @@ public class SeleniumIT {
         // start Morp (auth proxy) in that network
         morpContainer = new GenericContainer(DockerImageName.parse("ghcr.io/jaconi-io/morp:latest"))
                 .withNetwork(network)
-                // TODO consider multiple alias to demonstrate tenant extraction from host
-                .withNetworkAliases("morp")
+                .withNetworkAliases("morp", "tenant1-morp")
                 .withExposedPorts(8081, 8082)
                 .withEnv("SPRING_PROFILES_ACTIVE", "test")
                 .withFileSystemBind(
@@ -98,11 +93,10 @@ public class SeleniumIT {
                 .withLogConsumer(new Slf4jLogConsumer(DockerLoggerFactory.getLogger(morpContainer.getDockerImageName())))
                 .start();
 
-        System.out.println("Selenium remote URL is: " + seleniumContainer.getSeleniumAddress());
-        System.out.println("VNC URL is: " + seleniumContainer.getVncAddress());
         System.out.println("Keycloak: " + keycloakContainer.getAuthServerUrl());
         System.out.println("Morp server port: " + morpContainer.getMappedPort(8081));
         System.out.println("Morp actuator port: " + morpContainer.getMappedPort(8082));
+        System.out.println("VNC URL is: " + seleniumContainer.getVncAddress());
     }
 
     @BeforeEach
@@ -110,17 +104,6 @@ public class SeleniumIT {
 
         // point our mockserver client to mockserver running in the container
         mockServerClient = new MockServerClient(mockserverContainer.getHost(), mockserverContainer.getServerPort());
-        mockServerClient.reset();
-
-        // get the web driver into the Selenium container
-        driver = seleniumContainer.getWebDriver();
-        // use implicit wait of 10s (for DOM to build) when asking for page elements
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-    }
-
-    @Test
-    // TODO consider making this a parameterized test (passing in provider + tenant to test)
-    void testHappyPath() {
 
         // setup upstream behavior via mockserver client
         mockServerClient
@@ -132,8 +115,35 @@ public class SeleniumIT {
                         .withStatusCode(200)
                         .withBody("<h1 id='test'>Hello from mockserver</h1>"));
 
+        // get the web driver into the Selenium container
+        driver = seleniumContainer.getWebDriver();
+        // use implicit wait of 10s (for DOM to build) when asking for page elements
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // hacky way to delete all cookies (cross domain) in Chrome
+        driver.manage().deleteAllCookies();
+        driver.get("chrome://settings/clearBrowserData");
+        driver.findElement(By.xpath("//settings-ui")).sendKeys(
+                Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.TAB, Keys.ENTER);
+    }
+
+    @AfterEach
+    void tearDown() {
+        mockServerClient.reset();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "tenant1, morp:8081, /upstream/tenant1, /test",
+            "tenant1, tenant1-morp:8081, /upstream, /test"
+    })
+    void testWithKeycloak(String tenant, String host, String prefix, String path) {
+
+        // TODO need to (somehow) delete all cookies (cross domain)
+        driver.manage().deleteAllCookies();
+
         // have browser access the protected upstream via Morp
-        driver.get("http://morp:8081/upstream/tenant1/test");
+        driver.get("http://" + host + prefix + path);
 
         // expect redirects to take us to the Keycloak login mast
         saveScreenshot("01-keycloak-login-mask.png");
@@ -152,12 +162,14 @@ public class SeleniumIT {
         mockServerClient.verify(
                 request()
                         .withMethod("GET")
-                        .withPath("/test")
+                        .withPath(path)
+                        // expect host header rewritten by proxy
                         .withHeader("host", "upstream:1080")
                         // expect gateway proxy headers
-                        .withHeader("x-forwarded-host", "morp:8081")
-                        .withHeader("x-forwarded-prefix", "/upstream/tenant1")
-                        .withHeader("x-forwarded-proto", "http")
+                        .withHeader("x-forwarded-host", host)
+                        .withHeader("x-forwarded-prefix", prefix)
+                        // expect custom tenant header
+                        .withHeader("x-tenant-id", tenant)
                         // expect security session cookie (really?)
                         .withCookie("SESSION", ".+"), VerificationTimes.once());
     }
