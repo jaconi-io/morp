c18b1176cdf2440d4eda1c4da1d9156f06063185
Gerrit Schlüter <gschlueter@jaconi.io>
2025-11-21 14:20:32 +0100

    feat: migrate to Spring Boot 4

--- src/main/java/io/jaconi/morp/MorpRuntimeHints.java
+++ src/main/java/io/jaconi/morp/MorpRuntimeHints.java
@@ -1,13 +1,9 @@
 package io.jaconi.morp;
 
-import io.netty.channel.epoll.EpollDatagramChannel;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.util.ClassUtils;
 
-import static org.springframework.aot.hint.MemberCategory.INVOKE_DECLARED_CONSTRUCTORS;
-import static org.springframework.aot.hint.MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS;
 import static org.springframework.aot.hint.MemberCategory.INVOKE_PUBLIC_METHODS;
 
 public class MorpRuntimeHints implements RuntimeHintsRegistrar {
@@ -17,11 +13,5 @@ public class MorpRuntimeHints implements RuntimeHintsRegistrar {
 
         // Generic hints
         hints.reflection().registerType(ClassUtils.resolveClassName("com.sun.management.OperatingSystemMXBean", classLoader), INVOKE_PUBLIC_METHODS);
-        hints.reflection().registerType(JwtDecoder.class, INVOKE_PUBLIC_CONSTRUCTORS);
-
-        // This should not be required anymore as https://github.com/netty/netty/pull/13279 is contained in netty
-        // 4.1.91.Final (which is used by Spring Boot 3.0.5). However, things go south without it, so I will keep it
-        // for now.
-        hints.reflection().registerType(EpollDatagramChannel.class, INVOKE_DECLARED_CONSTRUCTORS);
     }
 }
