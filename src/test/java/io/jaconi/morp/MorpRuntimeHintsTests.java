744ff8d72cadb26076e6757fc1ca0cc66fd94612
Gerrit Schlüter <gschlueter@jaconi.io>
2026-06-18 09:39:33 +0200

    fix: workaround for missing metadata in Hibernate Validator

--- src/test/java/io/jaconi/morp/MorpRuntimeHintsTests.java
+++ src/test/java/io/jaconi/morp/MorpRuntimeHintsTests.java
@@ -1,8 +1,12 @@
 package io.jaconi.morp;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.aot.hint.MemberCategory.ACCESS_DECLARED_FIELDS;
+import static org.springframework.aot.hint.MemberCategory.INVOKE_DECLARED_CONSTRUCTORS;
 import static org.springframework.aot.hint.predicate.RuntimeHintsPredicates.reflection;
 
+import org.hibernate.validator.internal.util.logging.Log_$logger;
+import org.hibernate.validator.internal.util.logging.Messages_$bundle;
 import org.junit.jupiter.api.Test;
 import org.springframework.aot.hint.RuntimeHints;
 
@@ -16,6 +20,10 @@ class MorpRuntimeHintsTests {
         new MorpRuntimeHints().registerHints(hints, getClass().getClassLoader());
 
         assertThat(reflection().onType(GatewayPredicates.class)).accepts(hints);
+        assertThat(hints.reflection().getTypeHint(Log_$logger.class).getMemberCategories())
+                .contains(INVOKE_DECLARED_CONSTRUCTORS, ACCESS_DECLARED_FIELDS);
+        assertThat(hints.reflection().getTypeHint(Messages_$bundle.class).getMemberCategories())
+                .contains(INVOKE_DECLARED_CONSTRUCTORS, ACCESS_DECLARED_FIELDS);
 
     }
 
